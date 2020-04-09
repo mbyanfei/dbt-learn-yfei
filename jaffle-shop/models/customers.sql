8bba513ccb6b41314cab996f3cd8a6ff9d524ba4
Yan Fei <yfei@franklinamerican.com>
2020-04-09 20:53:26 +0000

    created jaffle_shop folder and refactor/reorganize code into this folder and create source oobject and reference it, add tests/docs

--- jaffle-shop/models/customers.sql
+++ jaffle-shop/models/customers.sql
@@ -6,11 +6,11 @@
 
 with customers as (
     select *
-    from {{ref('stg_customers')}}
+    from {{ref('stg_jaffle_shop__customers')}}
 ),
 orders as (
     select *
-    from {{ref('stg_orders')}}
+    from {{ref('stg_jaffle_shop__orders')}}
 ),
 payments as (
     select *
